04daf88e0733d51ec42e68614bb57aae39144498
Kazumasa Utashiro <kaz@utashiro.com>
2022-02-24 17:05:15 +0900

    minil new App::Greple::wordle

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Greple::wordle - It's new $module
+
+# SYNOPSIS
+
+    use App::Greple::wordle;
+
+# DESCRIPTION
+
+App::Greple::wordle is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/App/Greple/wordle.pm
@@ -0,0 +1,39 @@
+package App::Greple::wordle;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Greple::wordle - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Greple::wordle;
+
+=head1 DESCRIPTION
+
+App::Greple::wordle is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
