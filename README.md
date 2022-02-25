bc814d296f0d7ca962ba507afd82a4a8737beef9
Kazumasa Utashiro <kaz@utashiro.com>
2022-02-25 13:26:46 +0900

    introduce compatible mode

--- README.md
+++ README.md
@@ -11,10 +11,27 @@ greple -Mwordle
 App::Greple::wordle is a greple module which implements wordle game.
 Correctness is checked by regular expression.
 
+Rule is almost same as original wordle but answer is different.  Daily
+answer is updated 0AM localtime.
+
 # BUGS
 
 Wrong position character is colored yellow always, even if it is
 colored green in other position.
+
+# ENVIRONMENT
+
+- WORDLE\_ANSWER
+
+    Set answer word.
+
+- WORDLE\_RANDOM
+
+    Generate random answer every time.
+
+- WORDLE\_COMPAT
+
+    Generate compatible answer with original game.
 
 # AUTHOR
 
--- lib/App/Greple/wordle.pm
+++ lib/App/Greple/wordle.pm
@@ -6,6 +6,7 @@ use utf8;
 our $VERSION = "0.01";
 
 use Data::Dumper;
+use Date::Calc qw(Delta_Days);
 use charnames ':full';
 use Getopt::EX::Colormap 'colorize';
 use App::Greple::wordle::word_all    qw(%word_all);
@@ -14,8 +15,10 @@ use App::Greple::wordle::word_hidden qw(@word_hidden);
 my $try = 6;
 my $answer = $ENV{WORDLE_ANSWER};
 my $random = $ENV{WORDLE_RANDOM};
+my $compat = $ENV{WORDLE_COMPAT};
 my $msg_correct = "\N{PARTY POPPER}";
 my $msg_wrong   = "\N{COLLISION SYMBOL}";
+my @answers;
 
 sub initialize {
     my($mod, $argv) = @_;
@@ -23,11 +26,13 @@ sub initialize {
     push @$argv, '--interactive', ('/dev/stdin') x 30
 	if -t STDIN;
 
-    unless ($random) {
-	my($year, $yday) = (localtime(time))[5,7];
-	srand($year * 1000 + $yday);
+    my($mday, $mon, $year, $yday) = (localtime(time))[3,4,5,7];
+    my $index = Delta_Days(2021, 6, 19, $year + 1900, $mon + 1, $mday);
+    unless ($compat) {
+	srand($index) unless $random;
+	$index = int rand(@word_hidden);
     }
-    $answer ||= $word_hidden[ rand(@word_hidden) ];
+    $answer ||= $word_hidden[ $index ];
     length($answer) == 5 or die "$answer: wrong word\n";
 
     my $green = do {
@@ -54,6 +59,7 @@ sub check {
 	say $msg_wrong;
 	$_ = '';
     } else {
+	push @answers, $_;
 	$try--;
     }
 }
@@ -90,10 +96,31 @@ greple -Mwordle
 App::Greple::wordle is a greple module which implements wordle game.
 Correctness is checked by regular expression.
 
+Rule is almost same as original wordle but answer is different.  Daily
+answer is updated 0AM localtime.
+
 =head1 BUGS
 
 Wrong position character is colored yellow always, even if it is
 colored green in other position.
+
+=head1 ENVIRONMENT
+
+=over 7
+
+=item WORDLE_ANSWER
+
+Set answer word.
+
+=item WORDLE_RANDOM
+
+Generate random answer every time.
+
+=item WORDLE_COMPAT
+
+Generate compatible answer with original game.
+
+=back
 
 =head1 AUTHOR
 
@@ -119,3 +146,5 @@ option --interactive \
        --begin    &__PACKAGE__::check   \
        --end      &__PACKAGE__::inspect \
        --epilogue &__PACKAGE__::show_answer
+
+#  LocalWords:  greple wordle localtime COMPAT Kazumasa Utashiro
